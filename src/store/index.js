caf19aaa35381da84bd48996a7cb4670c0cf5667
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-05-25 19:45:35 +0900

    make state with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    drawer: false,
+  },
+  mutations: {
+    // mutationの関数にはstateが自動で渡される
+    toggleSideMenu(state) {
+      state.drawer = !state.drawer;
+    },
+  },
+  actions: {
+    // actionではcontextが自動的に引数にわたってくる。
+    // contextのうち、commit関数だけを受け取る場合は以下のように記述する。
+    toggleSideMenu({ commit }) {
+      // mutationの関数を呼び出す際に使用される
+      commit("toggleSideMenu");
+    },
+  },
   modules: {},
 });
