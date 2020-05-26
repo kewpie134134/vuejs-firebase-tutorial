b581042bb80f3ddac1e673b7a96e5a7a50c0e00e
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-05-26 23:36:11 +0900

    setting for firebase by vuejs

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBs-8zcRPBHRO20tdP3AoA6dQ-23k7lxNk",
+  authDomain: "my-address-pj-c0a8f.firebaseapp.com",
+  databaseURL: "https://my-address-pj-c0a8f.firebaseio.com",
+  projectId: "my-address-pj-c0a8f",
+  storageBucket: "my-address-pj-c0a8f.appspot.com",
+  messagingSenderId: "662447248946",
+  appId: "1:662447248946:web:aa24f1449e769610068e2b",
+  measurementId: "G-0L6ZJ0HBJS",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
